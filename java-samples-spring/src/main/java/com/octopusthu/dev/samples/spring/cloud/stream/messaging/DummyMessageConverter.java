1eda1d31b2c553e8c1928fa4e5dddd285d96e5b4
octopusthu <octopusthu@gmail.com>
2023-12-17 21:14:19 +0800

    WIP: Spring Cloud Stream sample

--- java-samples-spring/src/main/java/com/octopusthu/dev/samples/spring/cloud/stream/messaging/DummyMessageConverter.java
+++ java-samples-spring/src/main/java/com/octopusthu/dev/samples/spring/cloud/stream/messaging/DummyMessageConverter.java
@@ -1,18 +1,22 @@
 package com.octopusthu.dev.samples.spring.cloud.stream.messaging;
 
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.converter.AbstractMessageConverter;
 
-public class BarMessageConverter extends AbstractMessageConverter {
+public class DummyMessageConverter extends AbstractMessageConverter {
     @Override
     protected boolean supports(Class<?> clazz) {
-        return (BarMessagePayload.class.equals(clazz));
+        return (DummyMessagePayload.class.isAssignableFrom(clazz));
     }
 
     @Override
     protected Object convertFromInternal(Message<?> message, Class<?> targetClass, Object conversionHint) {
-        Object payload = message.getPayload();
-//        return (payload instanceof BarMessagePayload ? payload : new Bar((byte[]) payload));
+        return message.getPayload();
+    }
+
+    @Override
+    protected Object convertToInternal(Object payload, MessageHeaders headers, Object conversionHint) {
         return payload;
     }
 }
---
+++ java-samples-spring/src/main/java/com/octopusthu/dev/samples/spring/cloud/stream/messaging/DummyMessageConverterConfiguration.java
@@ -0,0 +1,12 @@
+package com.octopusthu.dev.samples.spring.cloud.stream.messaging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DummyMessageConverterConfiguration {
+    @Bean
+    DummyMessageConverter dummyMessageConverter() {
+        return new DummyMessageConverter();
+    }
+}
